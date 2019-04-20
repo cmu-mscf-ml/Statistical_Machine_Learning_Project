4bb5e337c7a22c1f00bd046f1512e22fcad4cd3f
yczhangCecilia <49764666+yczhangCecilia@users.noreply.github.com>
2019-04-20 15:52:45 -0400

    Update factor_bid.py

--- factors/factor_bid.py
+++ factors/factor_bid.py
@@ -5,7 +5,7 @@ calculate each stock's closing bid price of each tick
 
 import numpy as np
 import pandas as pd
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time
 
 param = {'path_data': 'F:\\Class - Statistical Machine Learning II\\project\\'
                       + 'HFT\\1_data_cleaning\\L1.cleaned.20190404.csv',
@@ -15,6 +15,7 @@ param = {'path_data': 'F:\\Class - Statistical Machine Learning II\\project\\'
 dataset = pd.read_csv(param['path_data'], header=0, index_col=0)
 dataset['time'] = dataset['time'].apply(lambda t: datetime.strptime(t,
                                                    '%Y-%m-%d %H:%M:%S.%f'))
+dataset['h_m'] = [time(t.hour, t.minute) for t in dataset['time']]
 stocks = np.array(dataset['symbol'].unique())
 ticks = datetime(2019,4,4,9,30,0,0) + np.arange(23400)*timedelta(0,1,0)
 delta_t = timedelta(0,1,0)
@@ -22,4 +23,10 @@ delta_t = timedelta(0,1,0)
 for stock in stocks:
     subdata = dataset.loc[dataset['symbol']==stock]
     subdata.index = range(len(subdata))
-    
+    test.groupby(by='a').last()['b']
+    tick_end_ind = [list(subdata['time']>=t+delta_t).index(True)-1 for t in ticks]
+
+
+
+test = pd.DataFrame([[1,2,3],[1,5,6],[1,5,2],[2,3,6],[2,3,5],[2,6,2]], columns=['a','b','c'])
+test.groupby(by=['a','b']).last()

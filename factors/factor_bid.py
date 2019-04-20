aadb5822c45220e9ae17ef7f26f94424d9b1a41b
yczhangCecilia <49764666+yczhangCecilia@users.noreply.github.com>
2019-04-20 15:21:55 -0400

    Update factor_bid.py

--- factors/factor_bid.py
+++ factors/factor_bid.py
@@ -2,5 +2,20 @@
 
 import numpy as np
 import pandas as pd
-param = {'path': 'F:\\Class - Statistical Machine Learning II\\project\\HFT\\'}
+from datetime import datetime, timedelta
 
+param = {'path_data': 'F:\\Class - Statistical Machine Learning II\\project\\'
+                      + 'HFT\\1_data_cleaning\\L1.cleaned.20190404.csv',
+         'path_project': 'F:\\Class - Statistical Machine Learning II\\'
+                      + 'project\\HFT\\Statistical_Machine_Learning_Project'}
+
+dataset = pd.read_csv(param['path_data'], header=0, index_col=0)
+dataset['time'] = dataset['time'].apply(lambda t: datetime.strptime(t,
+                                                   '%Y-%m-%d %H:%M:%S.%f'))
+stocks = np.array(dataset['symbol'].unique())
+delta_t = 
+
+datetime(2019,4,4,9,30,0,0) + np.arange(23400)*delta_t
+ticks = np.arange(datetime(2019,4,4,9,30,0,0), 
+                  datetime(2019,4,4,16,0,0,0), 
+                  timedelta(0,1,0))

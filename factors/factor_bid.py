3ac6344ce7aabbc975359be7355119b61062c4d7
yczhangCecilia <49764666+yczhangCecilia@users.noreply.github.com>
2019-04-20 15:23:40 -0400

    Update factor_bid.py

--- factors/factor_bid.py
+++ factors/factor_bid.py
@@ -15,7 +15,4 @@ dataset['time'] = dataset['time'].apply(lambda t: datetime.strptime(t,
 stocks = np.array(dataset['symbol'].unique())
 delta_t = 
 
-datetime(2019,4,4,9,30,0,0) + np.arange(23400)*delta_t
-ticks = np.arange(datetime(2019,4,4,9,30,0,0), 
-                  datetime(2019,4,4,16,0,0,0), 
-                  timedelta(0,1,0))
+ticks = datetime(2019,4,4,9,30,0,0) + np.arange(23400)*timedelta(0,1,0)

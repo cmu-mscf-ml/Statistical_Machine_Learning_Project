1a26316569da62dc08d0813db89cb8fe5a1023e6
yczhangCecilia <49764666+yczhangCecilia@users.noreply.github.com>
2019-04-20 16:35:06 -0400

    Update factor_bid.py

--- factors/factor_bid.py
+++ factors/factor_bid.py
@@ -15,18 +15,13 @@ param = {'path_data': 'F:\\Class - Statistical Machine Learning II\\project\\'
 dataset = pd.read_csv(param['path_data'], header=0, index_col=0)
 dataset['time'] = dataset['time'].apply(lambda t: datetime.strptime(t,
                                                    '%Y-%m-%d %H:%M:%S.%f'))
-dataset['h_m'] = [time(t.hour, t.minute) for t in dataset['time']]
+dataset['h_m_s'] = [time(t.hour, t.minute, t.second) for t in dataset['time']]
 stocks = np.array(dataset['symbol'].unique())
-ticks = datetime(2019,4,4,9,30,0,0) + np.arange(23400)*timedelta(0,1,0)
+ticks = datetime(2019,4,4,9,30,0,0) + np.arange(int(6.5*3600))*timedelta(0,1,0)
 delta_t = timedelta(0,1,0)
 
-for stock in stocks:
-    subdata = dataset.loc[dataset['symbol']==stock]
-    subdata.index = range(len(subdata))
-    test.groupby(by='a').last()['b']
-    tick_end_ind = [list(subdata['time']>=t+delta_t).index(True)-1 for t in ticks]
 
+bid = dataset.groupby(by=['symbol','h_m_s']).last()['bid']
+bid = bid.reset_index()
+bid = bid.pivot(index='h_m_s',columns='symbol',values='bid')
 
-
-test = pd.DataFrame([[1,2,3],[1,5,6],[1,5,2],[2,3,6],[2,3,5],[2,6,2]], columns=['a','b','c'])
-test.groupby(by=['a','b']).last()

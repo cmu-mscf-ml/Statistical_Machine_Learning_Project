fff6d9d02de41bba6b4aad805631702114c21b73
yczhangCecilia <49764666+yczhangCecilia@users.noreply.github.com>
2019-04-21 23:45:02 -0400

    Create all_factors.py

---
+++ factors/all_factors.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+'''
+Summaries all factors of each stock
+'''
+
+import numpy as np
+import pandas as pd
+import os
+from datetime import datetime, timedelta, time
+
+param = {'path_factors': 'F:\\Class - Statistical Machine Learning II\\'
+                      + 'project\\HFT\\Statistical_Machine_Learning_Project\\'
+                      + 'factors',
+         'path_output': 'F:\\Class - Statistical Machine Learning II\\'
+                      + 'project\\HFT\\Statistical_Machine_Learning_Project\\'
+                      + 'factors\\all_factors'}
+
+
+factors = [f for f in os.listdir(param['path_factors']) if '.' not in f]
+factors.remove('all_factors')
+stocks = [s[:-4] for s in os.listdir(param['path_factors']+'\\'+factors[0])]
+
+for s in stocks:
+    stock_factors = None
+    for f in factors:
+        path = '\\'.join([param['path_factors'],f,s]) + '.csv'
+        f_data = pd.read_csv(path, header=0, index_col=0)
+        f_data.columns = [f]

c3e63d72860876407d9ff579d88907f437c76550
SilenceHan <31155890+SilenceHan@users.noreply.github.com>
2019-04-26 15:28:13 -0400

    5s research

--- Final Code/Model_5s.py
+++ Final Code/Model_5s.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import ElasticNet
 from sklearn.svm import SVR
+from sklearn.linear_model import Ridge
 
 param = {'path_model_data': ####### fill your path here ######
                  'D:\\'
@@ -41,25 +42,27 @@ def evaluate_model(y_test_fit, y_test, y_train_fit=None, y_train=None):
     result['test'] = test_result
     return result
     
-
+def stock_process(stock, factors, y_horizon, param):
+    # fetch stock's modeling data
+    data = pd.read_csv(param['path_model_data']+stock+'.csv', index_col=0)
+    fmt = '%Y-%m-%d %H:%M:%S'
+    data['h_m_s'] = data['h_m_s'].apply(lambda x: datetime.strptime(x,fmt))
+    data.index = data['h_m_s']
+    del data['h_m_s']
+    data = data.dropna()
+    X = data.loc[:,factors]
+    ret_name = '_'.join(['fut',str(int(y_horizon)),'ret'])
+    y = data[ret_name]
+    return {'X':X,'y':y}
+    
 def run_model(stocks, factors, y_horizon, model, 
               param, rolling=False, **model_param):
     all_result = []
     for stock in stocks:
-
-        # fetch stock's modeling data
-        data = pd.read_csv(param['path_model_data']+stock+'.csv', index_col=0)
-        fmt = '%Y-%m-%d %H:%M:%S'
-        data['h_m_s'] = data['h_m_s'].apply(lambda x: datetime.strptime(x,fmt))
-        data.index = data['h_m_s']
-        del data['h_m_s']
-        data = data.dropna()
-        X = data.loc[:,factors]
-        ret_name = '_'.join(['fut',str(int(y_horizon)),'ret'])
-        y = data[ret_name]
-        
+        tmp = stock_process(stock, factors, y_horizon, param)
+        X = tmp['X']
+        y = tmp['y']
         # run model
-
         n_obs = len(X)
         n_train = int(n_obs*param['train_ratio'])
         if rolling:
@@ -129,18 +132,17 @@ y_horizon = 5
 
 
 
-
-
-
 #############  Research ####################
 '''
 The code below is process-oriented and can be a litte messy
 There are some code re-using (copy and paste)
 
-
-First try random forest.
-It's complex and have selection power.
+Actually, this part is more appropriate for a jupyter notebook format,
+if have time, I can move this to jupyter....  
 '''
+
+
+#### 1. Try random forest for feature selectuon
 model = RandomForestRegressor
 model_name = 'random_forest'
 model_param = {'max_depth': 2, 'random_state': 0, 'n_estimators': 100}
@@ -168,25 +170,21 @@ key_results.name = model_name+'_'+str(model_param)+'_'+str(y_horizon)+" days"
 # is even lower than 0.01
 stock_subset = key_results.loc[key_results['Score_train']>0.01]['Stock']
 
+print(key_results)
 print(stock_subset)
 # ACB, CRON, ECA, FOOD, PVG, SHOP, TECK.B are good
 
-# Look at each’s feature importance
+###### 2. For the selected stocks, look at each’s feature importance
 factors = all_factors
 subset_importance = pd.DataFrame(columns=factors,
                                  data=np.zeros((len(stock_subset),len(factors))))
+
 subset_importance.index = stock_subset.values
 for stock in stock_subset.values:
-    # fetch stock's modeling data
-    data = pd.read_csv(param['path_model_data']+stock+'.csv', index_col=0)
-    fmt = '%Y-%m-%d %H:%M:%S'
-    data['h_m_s'] = data['h_m_s'].apply(lambda x: datetime.strptime(x,fmt))
-    data.index = data['h_m_s']
-    del data['h_m_s']
-    data = data.dropna()
-    X = data.loc[:,factors]
-    ret_name = '_'.join(['fut',str(int(y_horizon)),'ret'])
-    y = data[ret_name]
+    # process data
+    tmp = stock_process(stock, factors, y_horizon, param)
+    X = tmp['X']
+    y = tmp['y']
         
     # run model
     n_obs = len(X)
@@ -199,8 +197,106 @@ for stock in stock_subset.values:
     reg.fit(X_train, y_train)
     subset_importance.loc[stock] = reg.feature_importances_
 
-# select the ones with feature importances higher than 0.01
+print(subset_importance)
+
+# we can select the ones with feature importances higher than 0.01
 # for the chosen model, we use random forest to refit it
+# we can also considerother models at this stage
+
+### 2.1. Random forest for selected variables
+'''
+Try to tune the model, but seems that tuning cannot help so much
+Look at the test result, the promising stocks include 'ACB', 'CRON', 'PVG'
+'''
+sub_stocks = stock_subset.values
+sub_results = []
+
+model = RandomForestRegressor
+model_name = 'random_forest'
+model_param = {'max_depth': 2, 'random_state': 0, 'n_estimators': 100}
+
+for stock in sub_stocks:
+    # print(stock)
+    subset_features = subset_importance.columns[subset_importance.loc[stock]>0.01].values
+    sub_results.append(run_model([stock], subset_features, y_horizon, model, param, **model_param)[0]) 
+
+sub_key_results = [[res['stock'],res['train']['score'],res['train']['sharpe'],
+                res['test']['score'],res['test']['sharpe']] for res in sub_results]
+
+sub_key_results = pd.DataFrame(columns=['Stock','Score_train','Sharpe_train',
+                                    'Score_test','Sharpe_test'],data=sub_key_results)
+print(sub_key_results)
+
+'''
+Further look at the feature imporances, we find that there are 
+one to three factors that have much higher importances than other factors,
+so in the refitting, we only consider these factors 
+
+We provide a function here becasue we need to use the same procedure many times
+'''
+
+def report_single_stock(stock, factors, model, **model_param):
+    tmp = stock_process(stock, factors, y_horizon, param)
+    X = tmp['X']
+    y = tmp['y']
+    # run model
+    n_obs = len(X)
+    n_train = int(n_obs*param['train_ratio'])
+    X_train, y_train = X.iloc[:n_train].copy(), y[:n_train].copy()
+    X_test, y_test = X.iloc[n_train:].copy(), y[n_train:].copy()
+    reg = model(**model_param)
+    reg.fit(X_train, y_train)
+    y_train_fit = reg.predict(X_train)
+    y_test_fit = reg.predict(X_test)
+    result = evaluate_model(y_test_fit, y_test, y_train_fit, y_train)
+    ## result demo ##
+    plt.plot(np.cumsum(result['train']['pnl']),label='train')
+    train_revenue = np.cumsum(result['train']['pnl'])[-1]
+    plt.plot(train_revenue+np.cumsum(result['test']['pnl']),label='test')
+    plt.title(stock + ", PnL")
+    plt.legend()
+    plt.show()
+    
+    print(stock+":")
+    print("Train Score: ", round(result['train']['score'],5))
+    print("Train Sharpe: ", round(result['train']['sharpe'],5))
+    print("Test Score: ", round(result['test']['score'],5))
+    print("Test Sharpe: ", round(result['test']['sharpe'],5))
+
+
+    return reg, result
+
+# model parameter
+model = RandomForestRegressor
+model_param = {'max_depth': 2, 'random_state': 0, 'n_estimators': 100}
+
+### 2.1.1 ACB 
+stock = 'ACB'
+factors = ['volum_imbalance']
+
+rf_ACB = report_single_stock(stock, factors, model, **model_param)
+
+### 2.1.2 CRON
+stock = 'CRON'
+factors = ['mid_momentum_50ord', 'spread_30s', 'volum_imbalance']
+
+rf_cron = report_single_stock(stock, factors, model, **model_param)
+
+########## PVG
+stock = 'PVG'
+factors = ['mid_momentum_50ord', 'mid_snapshot', 'spread_10s']
+
+rf_PVG = report_single_stock(stock, factors, model, **model_param)
+
+### 2.2. Elastic Net for selected variables
+'''
+Try to tune the model, find alpha = 1e-6 seems to be promising
+Look at the test result, the promising stocks include “ACB”, “ECA”, “PVG” 
+Similar but a little different from rf.
+'''
+model = ElasticNet
+model_param = {'alpha':1e-6, 'normalize': True}
+
 sub_stocks = stock_subset.values
 sub_results = []
 
@@ -215,151 +311,44 @@ sub_key_results = [[res['stock'],res['train']['score'],res['train']['sharpe'],
 sub_key_results = pd.DataFrame(columns=['Stock','Score_train','Sharpe_train',
                                     'Score_test','Sharpe_test'],data=sub_key_results)
 
+print (sub_key_results)
 
-
-################## single research ##########
-######### ACB
+### 2.2.1. 'ACB'
 stock = 'ACB'
+factors = subset_importance.columns[subset_importance.loc[stock]>0.01].values
+en_ACB = report_single_stock(stock, factors, model, **model_param)
+print(en_ACB[0].coef_)
+# only the last 'volum_imbalance is non-zero'
+# use it to refit redge regression, we tune the parameter and find alpha =1 to
+# be good
 factors = ['volum_imbalance']
-model = RandomForestRegressor
-model_name = 'random_forest'
-model_param = {'max_depth': 2, 'random_state': 0, 'n_estimators': 100}
-# fetch stock's modeling data
-data = pd.read_csv(param['path_model_data']+stock+'.csv', index_col=0)
-fmt = '%Y-%m-%d %H:%M:%S'
-data['h_m_s'] = data['h_m_s'].apply(lambda x: datetime.strptime(x,fmt))
-data.index = data['h_m_s']
-del data['h_m_s']
-data = data.dropna()
-X = data.loc[:,factors]
-ret_name = '_'.join(['fut',str(int(y_horizon)),'ret'])
-y = data[ret_name]
-    
-# run model
-n_obs = len(X)
-n_train = int(n_obs*param['train_ratio'])
-
-X_train, y_train = X.iloc[:n_train].copy(), y[:n_train].copy()
-X_test, y_test = X.iloc[n_train:].copy(), y[n_train:].copy()
-            
-reg = model(**model_param)
-reg.fit(X_train, y_train)
-y_train_fit = reg.predict(X_train)
-y_test_fit = reg.predict(X_test)
-result = evaluate_model(y_test_fit, y_test, y_train_fit, y_train)
+ridge_ACB = report_single_stock(stock, factors, Ridge, alpha=1, normalize=True)
 
 
-## result demo ##
-
-plt.plot(np.cumsum(result['train']['pnl']),label='train')
-train_revenue = np.cumsum(result['train']['pnl'])[-1]
-
-plt.plot(train_revenue+np.cumsum(result['test']['pnl']),label='test')
-plt.title("ACB, PnL")
-plt.legend()
-plt.show()
-
-print("ACB:")
-print("Train Score: ", round(result['train']['score'],5))
-print("Train Sharpe: ", round(result['train']['sharpe'],5))
-print("Test Score: ", round(result['test']['score'],5))
-print("Test Sharpe: ", round(result['test']['sharpe'],5))
-
-########## CRON
-stock = 'CRON'
-factors = ['mid_momentum_50ord', 'spread_30s', 'volum_imbalance']
-model = RandomForestRegressor
-model_name = 'random_forest'
-model_param = {'max_depth': 2, 'random_state': 0, 'n_estimators': 100}
-# fetch stock's modeling data
-data = pd.read_csv(param['path_model_data']+stock+'.csv', index_col=0)
-fmt = '%Y-%m-%d %H:%M:%S'
-data['h_m_s'] = data['h_m_s'].apply(lambda x: datetime.strptime(x,fmt))
-data.index = data['h_m_s']
-del data['h_m_s']
-data = data.dropna()
-X = data.loc[:,factors]
-ret_name = '_'.join(['fut',str(int(y_horizon)),'ret'])
-y = data[ret_name]
-    
-# run model
-n_obs = len(X)
-n_train = int(n_obs*param['train_ratio'])
-
-X_train, y_train = X.iloc[:n_train].copy(), y[:n_train].copy()
-X_test, y_test = X.iloc[n_train:].copy(), y[n_train:].copy()
+### 2.2.2. 'ECA'
+stock = 'ECA'
+factors = subset_importance.columns[subset_importance.loc[stock]>0.01].values
+en_ECA = report_single_stock(stock, factors, model, **model_param)
+print(en_ECA[0].coef_)
+# 'volum_imbalance' is the only selected factor, again we use ridge to refit it
+# and tune the parameter, again alpha = 1 is good
+factors = ['volum_imbalance']
+ridge_ECA = report_single_stock(stock, factors, Ridge, alpha=1, normalize=True)
 
 
-            
-reg = model(**model_param)
-reg.fit(X_train, y_train)
-y_train_fit = reg.predict(X_train)
-y_test_fit = reg.predict(X_test)
-result = evaluate_model(y_test_fit, y_test, y_train_fit, y_train)
-
-
-## result demo ##
-
-plt.plot(np.cumsum(result['train']['pnl']),label='train')
-train_revenue = np.cumsum(result['train']['pnl'])[-1]
-
-plt.plot(train_revenue+np.cumsum(result['test']['pnl']),label='test')
-plt.title("CRON, PnL")
-plt.legend()
-plt.show()
-
-print("Cron:")
-print("Train Score: ", round(result['train']['score'],5))
-print("Train Sharpe: ", round(result['train']['sharpe'],5))
-print("Test Score: ", round(result['test']['score'],5))
-print("Test Sharpe: ", round(result['test']['sharpe'],5))
-
-########## PVG
+### 2.2.3. 'PVG'
 stock = 'PVG'
-factors = ['mid_momentum_50ord', 'mid_snapshot', 'spread_10s']
-model = RandomForestRegressor
-model_name = 'random_forest'
-model_param = {'max_depth': 2, 'random_state': 0, 'n_estimators': 100}
-# fetch stock's modeling data
-data = pd.read_csv(param['path_model_data']+stock+'.csv', index_col=0)
-fmt = '%Y-%m-%d %H:%M:%S'
-data['h_m_s'] = data['h_m_s'].apply(lambda x: datetime.strptime(x,fmt))
-data.index = data['h_m_s']
-del data['h_m_s']
-data = data.dropna()
-X = data.loc[:,factors]
-ret_name = '_'.join(['fut',str(int(y_horizon)),'ret'])
-y = data[ret_name]
-    
-# run model
-n_obs = len(X)
-n_train = int(n_obs*param['train_ratio'])
-
-X_train, y_train = X.iloc[:n_train].copy(), y[:n_train].copy()
-X_test, y_test = X.iloc[n_train:].copy(), y[n_train:].copy()
-
-            
-reg = model(**model_param)
-reg.fit(X_train, y_train)
-y_train_fit = reg.predict(X_train)
-y_test_fit = reg.predict(X_test)
-result = evaluate_model(y_test_fit, y_test, y_train_fit, y_train)
+factors = subset_importance.columns[subset_importance.loc[stock]>0.01].values 
+en_PVG = report_single_stock(stock, factors, model, **model_param)
+print(en_PVG[0].coef_)
+# again, 'volum_imbalance' is the only selected factor, again we use ridge to refit it
+# and tune the parameter, again alpha = 0.1 is good
+factors = ['volum_imbalance']
+ridge_PVG = report_single_stock(stock, factors, Ridge, alpha=1e-1, normalize=True)
 
 
-## result demo ##
 
-plt.plot(np.cumsum(result['train']['pnl']),label='train')
-train_revenue = np.cumsum(result['train']['pnl'])[-1]
 
-plt.plot(train_revenue+np.cumsum(result['test']['pnl']),label='test')
-plt.title("PVG, PnL")
-plt.legend()
-plt.show()
 
-print("PVG:")
-print("Train Score: ", round(result['train']['score'],5))
-print("Train Sharpe: ", round(result['train']['sharpe'],5))
-print("Test Score: ", round(result['test']['score'],5))
-print("Test Sharpe: ", round(result['test']['sharpe'],5))
 
 ##### Test on Apr. 5
